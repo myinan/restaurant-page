1096991063266550a45e7f92468a15956da512d0
myinan <m.yasir.inan@gmail.com>
2023-12-11 21:48:34 +0300

    Create seperate modules

--- src/module-header-footer/moduleHeaderFooter.js
+++ src/module-header-footer/moduleHeaderFooter.js
@@ -1,5 +1,6 @@
 //Create header
 const headerDiv = document.getElementById("header-container");
+const containerDiv = document.createElement("div");
 
 function appendButtons() {
     const name = document.createElement("p");
@@ -11,15 +12,24 @@ function appendButtons() {
 
     for (let i = 0; i < 3; i++) {
         let newButton = document.createElement("button");
-        headerDiv.appendChild(newButton);
+        containerDiv.appendChild(newButton);
         buttons.push(newButton);
     }
 
+    headerDiv.appendChild(containerDiv);
     buttons[0].innerText = "Home";
     buttons[1].innerText = "Menu";
     buttons[2].innerText = "Contact";
 }
 
+function styleHeader() {
+    headerDiv.classList.add("header-top-container");
+    containerDiv.classList.add("header-buttons-container");
+}
+
 export function addHeaderFooterContent() {
-    document.addEventListener("DOMContentLoaded", appendButtons);
+    document.addEventListener("DOMContentLoaded",() => {
+        appendButtons();
+        styleHeader();
+    });
 };
