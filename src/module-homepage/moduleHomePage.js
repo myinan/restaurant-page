3db1955940baba9072ec691ed1b4265fa61c5e9e
myinan <m.yasir.inan@gmail.com>
2023-12-12 04:01:58 +0300

    Style homepage elements

--- src/module-homepage/moduleHomePage.js
+++ src/module-homepage/moduleHomePage.js
@@ -1,24 +1,27 @@
+
+
 const contentDiv = document.getElementById("main-container");
 
-function addBgImage() {
+function addStyling() {
     contentDiv.classList.add("content-bg");
+    contentDiv.classList.add("main-container");
 }
 
 function addHeader() {
-    const welcomeHeader = document.createElement("h1");
+    const welcomeHeader = document.createElement("div");
     welcomeHeader.innerText = "Welcome to our great Restaurant!";
     contentDiv.appendChild(welcomeHeader);
 }
 
 function addPara() {
-    const welcomePara = document.createElement("p");
+    const welcomePara = document.createElement("div");
     welcomePara.innerText = "You can find a vast list of choices avaliable to you in our beatiful Restaurant!";
     contentDiv.appendChild(welcomePara);
 }
 
 export function addMainContentHomepage() {
     document.addEventListener("DOMContentLoaded",() => {
-        addBgImage();
+        addStyling();
         addHeader();
         addPara();
     });
