84b61df9f136a2e21ceee20a16b4de94dad70435
myinan <m.yasir.inan@gmail.com>
2023-12-13 20:05:11 +0300

    Complete module menupage

--- src/module-menupage/moduleMenuPage.js
+++ src/module-menupage/moduleMenuPage.js
@@ -1,4 +1,25 @@
+import menuImage1 from "./assets/roasted-chicken.jpeg";
+
 const contentDiv = document.getElementById("main-container");
+
+function addMenuItems() {
+    for (let i = 0; i < 8; i++) {
+        const imageCopy = new Image();
+        imageCopy.src = menuImage1;
+
+        let newItem = document.createElement("div");
+        let newItemHeader = document.createElement("p");
+        newItemHeader.innerText = "Rotisserie chicken";
+        let newItemParaf = document.createElement("p");
+        newItemParaf.innerText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Phasellus dignissim tristique arcu sed hendrerit. 
+        Nullam mollis ex quis tellus efficitur, eget blandit sapien rutrum. 
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+        Sed bibendum auctor nisi ac convallis. Nam vitae quam sagittis, sollicitudin turpis nec, interdum dui.`;
+        newItem.append(imageCopy, newItemHeader, newItemParaf);
+        contentDiv.appendChild(newItem);
+    }
+}
 
 function addStyling() {
     contentDiv.classList.add("main-bg-menupage");
@@ -6,5 +27,6 @@ function addStyling() {
 }
 
 export function addMainContentMenupage() {
-        addStyling();
+    addMenuItems();
+    addStyling();
 };
