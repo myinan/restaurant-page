9ef8c61e41e1ff0e7673d1c35f9dc470b2adbc8b
myinan <m.yasir.inan@gmail.com>
2023-12-15 00:12:18 +0300

    Final version bundled

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,4 +4,20 @@ const { merge } = require('webpack-merge');
  module.exports = merge(common, {
    mode: 'production',
    devtool: 'source-map',
+   module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
  });
