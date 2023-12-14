97950797dd1efc4e3a73ea4a8d92c63e88b62121
myinan <m.yasir.inan@gmail.com>
2023-12-14 19:21:06 +0300

    Create contact module

--- src/module-contactpage/moduleContactPage.js
+++ src/module-contactpage/moduleContactPage.js
@@ -1,3 +1,7 @@
+import phonecallIcon from "./phone-call.png";
+import locationIcon from "./location.png";
+import emailIcon from "./email.jpg";
+
 const contentDiv = document.getElementById("main-container");
 
 const overlayContainer = document.createElement("div");
@@ -6,13 +10,51 @@ const parafFirst = document.createElement("p");
 const parafSecond = document.createElement("p");
 
 const infoContainer = document.createElement("div");
+let infoItems = [];
 for (let i = 0; i < 3; i++) {
     let infoItem = document.createElement("div");
+    infoItems.push(infoItem);
     infoContainer.appendChild(infoItem);
 }
 
 parafFirst.innerText = "Perfectly Balanced";
 parafSecond.innerText = "QUALITY & TASTE";
+
+function populateInfoItems() {
+    function populateLeftmostDiv() {
+        const leftmostIcon = new Image();
+        leftmostIcon.src = phonecallIcon;
+        const callUsText = document.createElement("p");
+        callUsText.innerText = "Call us";
+        const phoneNumber = document.createElement("p");
+        phoneNumber.innerText = "+90 123 456 78 90";
+        infoItems[0].append(leftmostIcon, callUsText, phoneNumber);
+    }
+
+    function populateMiddleDiv() {
+        const middleIcon = new Image();
+        middleIcon.src = locationIcon;
+        const locationText = document.createElement("p");
+        locationText.innerText = "Location";
+        const adress = document.createElement("p");
+        adress.innerText = "Kentmeydanı AVM, Bursa";
+        infoItems[1].append(middleIcon, locationText, adress);
+    }
+
+    function populateRightmostDiv() {
+        const rightmostIcon = new Image();
+        rightmostIcon.src = emailIcon;
+        const emailText = document.createElement("p");
+        emailText.innerText = "Email";
+        const mail = document.createElement("p");
+        mail.innerText = "restadelux@gmail.com";
+        infoItems[2].append(rightmostIcon, emailText, mail);
+    }
+
+    populateLeftmostDiv();
+    populateMiddleDiv();
+    populateRightmostDiv();
+}
 
 function addContent() {
     overlayContainer.append(parafFirst, parafSecond, infoContainer);
@@ -28,6 +70,8 @@ function addStyling() {
 }
 
 export function addMainContentContactpage() {
+    populateInfoItems();
+
     addContent();
     addStyling();
 }
